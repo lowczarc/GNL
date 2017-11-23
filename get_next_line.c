8c9f4c588d76e097ac411a8734792997de8e72f7
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2017-11-23 01:22:30 +0100

    GNL fini mais buggé

--- get_next_line.c
+++ get_next_line.c
@@ -6,59 +6,53 @@
 /*   By: lowczarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 13:15:40 by lowczarc          #+#    #+#             */
-/*   Updated: 2017/11/18 19:36:14 by lowczarc         ###   ########.fr       */
+/*   Updated: 2017/11/23 01:19:23 by lowczarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-static int	realloc_buff(char ***buff, int fd)
+static char	**realloc_buff(char ***buff, int fd)
 {
-	char	**ret;
-	int		i;
+	char			**ret;
+	static int		i = 0;
+	int				j;
 
-	i = 0;
-	while (*buff != NULL && (*buff)[i])
+	j = 0;
+	while (*buff != NULL && j < i)
+		j++;
+	i = ((j - 1 > fd) ? j : fd + 1);
+	if (j - 1 <= fd)
 	{
-		if (*((int*)(&(*buff)[i][BUFF_SIZE])) == fd)
-			return (i);
-		i++;
+		ret = (char**)ft_memalloc(sizeof(char*) * (i + 2));
+		while (--j > 0)
+			ret[j] = (*buff)[j];
+		ret[i + 1] = NULL;
 	}
-	ret = (char**)malloc(sizeof(char*) * (i + 2));
-	i = 0;
-	while (*buff != NULL && (*buff)[i])
+	else
+		ret = *buff;
+	if (!ret[fd])
 	{
-		ret[i] = (*buff)[i];
-		free((*buff)[i]);
-		i++;
+		ret[fd] = (char*)ft_memalloc(sizeof(char) * BUFF_SIZE);
+		read(fd, ret[fd], BUFF_SIZE);
 	}
-	ret[i] = (char*)ft_memalloc((sizeof(char) * BUFF_SIZE) + sizeof(int));
-	ret[i + 1] = NULL;
-	*((int*)(&ret[i][BUFF_SIZE])) = fd;
-	read(fd, ret[i], BUFF_SIZE);
-	if (*buff != NULL)
-		free(*buff);
-	*buff = ret;
-	return (i);
+	return (ret);
 }
 
 static int	buff_to_str(char **str, char *buff, int fd)
 {
-	int		i;
 	int		j;
 	char	*ret;
 
-	i = ft_strlen(*str);
 	j = 0;
 	while (j < BUFF_SIZE && buff[j] && buff[j] != '\n')
 		j++;
-	if (!(ret = ft_strnew(i + j)))
+	if (!(ret = ft_strnew(ft_strlen(*str) + j)))
 		return (-2);
 	ft_strcpy(ret, *str);
 	ft_strncat(ret, buff, j);
-	if (*str)
-		free(*str);
+	ft_memdel((void**)str);
 	*str = ret;
 	if (j == BUFF_SIZE)
 		return (0);
@@ -67,25 +61,26 @@ static int	buff_to_str(char **str, char *buff, int fd)
 	if (buff[j] == '\n')
 	{
 		ft_memmove(buff, &buff[j + 1], BUFF_SIZE - j - 1);
-		ft_bzero(&buff[BUFF_SIZE - j], j);
+		ft_bzero(&buff[BUFF_SIZE - j - 1], j + 1);
+		read(fd, &buff[BUFF_SIZE - j - 1], j + 1);
 		return (1);
 	}
+	return (-1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
 	static char	**buff = NULL;
-	int			buffid;
 	int			t;
 
-	buffid = realloc_buff(&buff, fd);
-	*line = strdup("");
+	buff = realloc_buff(&buff, fd);
+	*line = ft_strdup("");
 	while (1)
 	{
-		t = buff_to_str(line, buff[buffid], fd);
+		t = buff_to_str(line, buff[fd], fd);
 		if (t == 0)
 		{
-			if (read(fd, buff[buffid], BUFF_SIZE) == -1)
+			if (read(fd, buff[fd], BUFF_SIZE) == -1)
 				return (-1);
 		}
 		if (t == -1)
@@ -93,18 +88,4 @@ int			get_next_line(const int fd, char **line)
 		if (t == 1)
 			return (1);
 	}
-}
-
-int	main(int argc, char **argv)
-{
-	char	*str;
-	int		fd;
-
-	fd = open("test", O_RDONLY);
-	while (get_next_line(fd, &str))
-	{
-		ft_putstr(str);
-		ft_putchar('\n');
-	}
-	close(fd);
 }
